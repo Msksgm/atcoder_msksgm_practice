987ba0c65b209d3fdd4ca53ad5020abfa5d1f5c4
MskAerial <4419517@ed.tus.ac.jp>
2020-03-11 19:20:44 +0900

    fix

--- abc/149/B.py
+++ abc/149/B.py
@@ -1,10 +1,16 @@
 def main():
     a, b, k = map(int, input().split())
-    if a <= k:
+    if a == 0 and b == 0:
+        print(a, b)
+    elif a <= k:
         a_diff = k - a
         a = 0
-        b -= a_diff
-        print(a, b)
+        if b <= a_diff:
+            b = 0
+            print(a, b)
+        else:
+            b -= a_diff
+            print(a, b)
     else:
         a = a - k
         print(a, b)
