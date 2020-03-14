bbafa52e4f1185445896aba46339b772a481e27f
MskAerial <4419517@ed.tus.ac.jp>
2020-03-14 09:20:20 +0900

    fix

--- abc/123/B.py
+++ abc/123/B.py
@@ -1,24 +1,31 @@
 import numpy as np
 
 
+def my_round(val, digit=0):
+    p = 10 ** digit
+    return np.ceil((val * p * 2 + 1)) // 2 / p
+
+
 def main():
     dishes = [input() for _ in range(5)]
     first_digit = float("inf")
     order_dishies = []
-    print(dishes)
     for i in range(5):
-        first_digit = min(int(dishes[i][-1]), first_digit)
-        if first_digit == dishes[i][-1]:
-            order_dishies.append(i)
-        else:
+        if int(dishes[i][-1]) == 0:
             order_dishies.insert(0, i)
+        else:
+            first_digit = min(int(dishes[i][-1]), first_digit)
+            if first_digit == int(dishes[i][-1]):
+                order_dishies.append(i)
+            else:
+                order_dishies.insert(0, i)
     ans = 0
     for i in range(5):
-        if i != 4:
-            ans += np.ceil(order_dishies[i])
+        if i == 4:
+            ans += int(dishes[order_dishies[i]])
         else:
-            ans += order_dishies[i]
-    print(ans)
+            ans += my_round(int(dishes[order_dishies[i]]), -1)
+    print(int(ans))
 
 
 if __name__ == "__main__":
