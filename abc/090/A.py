feee1b4833d164da6bf4af8da6bb286770cb6c17
Msksgm <4419517@ed.tus.ac.jp>
2020-03-23 20:02:18 +0900

    first commit

---
+++ abc/090/A.py
@@ -0,0 +1,12 @@
+def main():
+    c1 = list(input())
+    c2 = list(input())
+    c3 = list(input())
+
+    ans = ''
+    ans += c1[0] + c2[1] + c3[2]
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
