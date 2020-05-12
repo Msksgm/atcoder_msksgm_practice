b34c3709b3652fae3a1f82d4bd22ab7009bfd8ac
Msksgm <4419517@ed.tus.ac.jp>
2020-05-12 20:51:11 +0900

    1wa

---
+++ abc/062/B.py
@@ -0,0 +1,16 @@
+def main():
+    b, w = map(int, input().split())
+    image = [list(input()) for _ in range(b)]
+    print(image)
+    image.insert(0, ["#"]*w)
+    image.append(["#"]*w)
+    for im in image:
+        im.insert(0, "#")
+        im.append("#")
+
+    for im in image:
+        print(*im, sep="")
+
+
+if __name__ == "__main__":
+    main()
