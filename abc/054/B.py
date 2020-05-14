4bbfdf686dae8fa924727cfeb87c2f7c40b3a059
Msksgm <4419517@ed.tus.ac.jp>
2020-05-14 21:31:25 +0900

    ac

--- abc/054/B.py
+++ abc/054/B.py
@@ -8,14 +8,14 @@ def main():
 
     As_arr = np.array(As)
     Bs_arr = np.array(Bs)
-    ans = 0
     for i in range(n-m+1):
         for j in range(n-m+1):
             xs, xe = i, i+m
             ys, ye = j, j+m
             if np.all(As_arr[ys:ye, xs:xe] == Bs_arr):
-                ans += 1
-    print(ans)
+                print('Yes')
+                exit()
+    print('No')
 
 
 if __name__ == "__main__":
