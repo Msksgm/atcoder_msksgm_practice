7b6c4955631c0f17fe556992506b210ae524f4a7
Msksgm <4419517@ed.tus.ac.jp>
2020-05-18 21:19:43 +0900

    ac

--- abc/168/D.py
+++ abc/168/D.py
@@ -1,10 +1,20 @@
-def search(rooms, li, anss, count):
-    count += 1
-    for l in li:
-        if anss[l] == 0:
-            anss[l] = count
-            search(rooms, rooms[l], anss, count)
-    return anss
+from collections import deque
+
+
+def bfs(rooms, v=1):
+    arrow = [-1] * len(rooms)
+    q = deque()
+    q.append(v)
+
+    while q:
+        now = q.popleft()
+        for next_v in rooms[now]:
+            if arrow[next_v] != -1:
+                continue
+            else:
+                q.append(next_v)
+                arrow[next_v] = now
+    return arrow
 
 
 def main():
@@ -14,16 +24,11 @@ def main():
         a, b = map(int, input().split())
         rooms[b].append(a) 
         rooms[a].append(b)
-    anss = [0]*(n+1)
-    li = rooms[1]
-    count = 0
-    anss = search(rooms, li, anss, count)
-    if 0 in anss[1:]:
-        print("No")
-    else:
-        print("Yes")
-        for ans in anss[1:]:
-            print(ans)
+
+    anss = bfs(rooms)
+    print("Yes")
+    for ans in anss[2:]:
+        print(ans)
 
 
 if __name__ == "__main__":
