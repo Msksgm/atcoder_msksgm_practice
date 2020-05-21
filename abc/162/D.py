c155c14bbbfa052a1e132394d7a079bbaaf0170a
Msksgm <4419517@ed.tus.ac.jp>
2020-05-21 20:43:05 +0900

    ac

--- abc/162/D.py
+++ abc/162/D.py
@@ -2,14 +2,12 @@ def main():
     n = int(input())
     s = list(input())
 
-    ans = 0
+    ans = s.count("R")*s.count("G")*s.count("B")
     for i in range(n):
         for j in range(i+1, n):
-            for k in range(j+1, n):
-                if j-i == k-j:
-                    continue
-                if s[i] != s[j] and s[j] != s[k] and s[k] != s[i]:
-                    ans += 1
+            k = j + j - i
+            if k < n and s[i] != s[j] and s[j] != s[k] and s[k] != s[i]:
+                ans -= 1
     print(ans)
 
 
