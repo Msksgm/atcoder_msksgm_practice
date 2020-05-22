3119471bbbcb2c02438f80630f12736a6a6b5e7a
Msksgm <4419517@ed.tus.ac.jp>
2020-05-22 21:00:40 +0900

    wa

--- abc/158/D.py
+++ abc/158/D.py
@@ -1,16 +1,32 @@
+from collections import deque
+
+
 def main():
-    s = input()
+    s = deque([input()])
     q = int(input())
+    rev = False
     for _ in range(q):
         query = input().split()
         if query[0] == "1":
-            s = s[::-1]
+            if not rev:
+                rev = True
+            else:
+                rev = False
         else:
             if query[1] == '1':
-                s = query[2] + s
+                pass
+                if rev:
+                    s.append(query[2])
+                else:
+                    s.appendleft(query[2])
             else:
-                s = s + query[2]
-    print(s)
+                if rev:
+                    s.appendleft(query[2])
+                else:
+                    s.append(query[2])
+    if rev:
+        s.reverse()
+    print(*s, sep="")
 
 
 if __name__ == "__main__":
