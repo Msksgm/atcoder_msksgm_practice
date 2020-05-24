2cc35d65c94f078a26309a4fea0f5199dd069360
Msksgm <4419517@ed.tus.ac.jp>
2020-05-24 19:44:54 +0900

    wa

--- abc/157/C.py
+++ abc/157/C.py
@@ -1,18 +1,29 @@
 def main():
     n, m = map(int, input().split())
-    ans = [0]*n
+    ans = [-1]*n
     for _ in range(m):
         s, c = map(int, input().split())
-        if ans[s-1] == 0:
+        if ans[s-1] == -1:
             ans[s-1] = c
-        elif ans[s-1] != 0 and ans[s-1] != c:
+        elif ans[s-1] != -1 and ans[s-1] != c:
             print(-1)
             exit()
 
-    if ans[0] == 0:
-        print(-1)
+    if n == 1:
+        if ans[0] == -1:
+            ans[0] = 0
     else:
-        print(*ans, sep="")
+        if ans[0] == -1:
+            print(-1)
+            exit()
+
+        if ans[0] == -1:
+            ans[0] = 1
+
+        for i in range(n):
+            if ans[i] == -1:
+                ans[i] = 0
+    print(*ans, sep="")
 
 
 if __name__ == "__main__":
