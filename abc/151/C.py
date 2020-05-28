ac3ebee982a4728d5cc22feba58a5ad92aef3bab
Msksgm <4419517@ed.tus.ac.jp>
2020-05-28 14:06:43 +0900

    ac

--- abc/151/C.py
+++ abc/151/C.py
@@ -2,20 +2,20 @@ def main():
     from sys import stdin
     input = stdin.readline
     N, M = map(int, input().split())
-    SP = [tuple(input().split()) for i in range(M)]
-    wa_num_temp = {}
-    ac_num_temp = {}
+    SP = [list(input().split()) for i in range(M)]
+    was = [0] * N
+    acs = [0] * N
+    penaltyCount = 0
     for sp in SP:
-        if sp[0] in list(ac_num_temp.keys()):
+        if acs[int(sp[0])-1] == 1:
             continue
-        if sp[0] not in list(wa_num_temp.keys()):
-            wa_num_temp[sp[0]] = 0
-        if sp[1] == "WA":
-            wa_num_temp[sp[0]] += 1
-        elif sp[1] == "AC":
-            ac_num_temp[sp[0]] = 1
+        if acs[int(sp[0])-1] == 0 and sp[1] == "WA":
+            was[int(sp[0])-1] += 1
+        elif acs[int(sp[0])-1] == 0 and sp[1] == "AC":
+            acs[int(sp[0])-1] = 1
+            penaltyCount += was[int(sp[0])-1]
 
-    print(sum(ac_num_temp.values()), sum(wa_num_temp.values()))
+    print(sum(acs), penaltyCount)
 
 
 if __name__ == "__main__":
