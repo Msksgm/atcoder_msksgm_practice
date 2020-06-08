725e6b22beab82087f86cf7565370e7004a554b5
Msksgm <4419517@ed.tus.ac.jp>
2020-06-08 15:08:19 +0900

    ac

--- abc/134/C.py
+++ abc/134/C.py
@@ -1,24 +1,14 @@
-import sys
-# import numpy as np
-from copy import deepcopy
-input = sys.stdin.readline
-
-
 def main():
-    N = int(input())
-    # A = np.array([int(input()) for _ in range(N)])
-    A = [int(input()) for _ in range(N)]
-    # print(A)
+    n = int(input())
+    As = [int(input()) for _ in range(n)]
 
-    for j in range(N):
-        B = deepcopy(A)
-        del B[j]
-        print(max(B))
-        # if j != A.index(max(A)):
-        #     print(max(A))
-        # else:
-        #     ans = [x for i, x in enumerate(A) if i != j]
-        #     print(max(ans))
+    As_sort = sorted(As)
+    second, first = As_sort[-2:]
+    for i in range(n):
+        if As[i] == first:
+            print(second)
+        else:
+            print(first)
 
 
 if __name__ == "__main__":
