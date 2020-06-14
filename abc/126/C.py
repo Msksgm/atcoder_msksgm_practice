7681280d55b6f06553599567a3bdd41e1321ab03
Msksgm <4419517@ed.tus.ac.jp>
2020-06-14 12:46:56 +0900

    first commit

--- abc/126/C.py
+++ abc/126/C.py
@@ -1,20 +1,5 @@
 def main():
-    N, K = map(int, [int(x) for x in input().split()])
-    # N, K = map(int,  input().split())
-    ans = 0
-    for n in range(1, N+1):
-        num = n
-        i = 0
-
-        while num < K:
-            num *= 2
-            # n *= 2
-            i += 1
-
-        # ans += 1/N*(0.5)**i
-        ans += ((1/2)**i)/N
-
-    print(ans)
+    n, k = map(int, input().split())
 
 
 if __name__ == "__main__":
