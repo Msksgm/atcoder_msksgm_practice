d09d713d837be539bd4ca04f2357fdca26ac8d58
Msksgm <4419517@ed.tus.ac.jp>
2020-06-14 13:00:27 +0900

    ac

--- abc/126/C.py
+++ abc/126/C.py
@@ -1,5 +1,14 @@
 def main():
     n, k = map(int, input().split())
+    ans = 0
+    for i in range(1, n+1):
+        temp = i
+        j = 0
+        while temp < k:
+            temp *= 2
+            j += 1
+        ans += (1/2)**j
+    print(ans/n)
 
 
 if __name__ == "__main__":
