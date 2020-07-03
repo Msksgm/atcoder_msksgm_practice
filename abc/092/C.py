dea3fbc7b33dfa163d9469c185d6e11213a2bd0f
Msksgm <4419517@ed.tus.ac.jp>
2020-07-03 12:38:21 +0900

    ac

---
+++ abc/092/C.py
@@ -0,0 +1,15 @@
+def main():
+    n = int(input())
+    As = [0] + list(map(int, input().split())) + [0]
+
+    cumsum = [0] * (n+1)
+    for i in range(1, n+2):
+        cumsum[i-1] = abs(As[i] - As[i-1])
+    cumsum_sum = sum(cumsum)
+    for i in range(1, n+1):
+        ans = cumsum_sum - cumsum[i-1] - cumsum[i] + abs(As[i+1] - As[i-1])
+        print(ans)
+
+
+if __name__ == "__main__":
+    main()
