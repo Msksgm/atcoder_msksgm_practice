8bb59d6b66bd137ce95cba2511fee738627b90f1
Msksgm <4419517@ed.tus.ac.jp>
2020-07-03 12:54:20 +0900

    ac

---
+++ abc/090/C.py
@@ -0,0 +1,16 @@
+def main():
+    n, m = map(int, input().split())
+
+    if n == 1 and m == 1:
+        ans = 1
+    elif n == 1:
+        ans = m - 2
+    elif m == 1:
+        ans = n - 2
+    else:
+        ans = n*m - (2*n + 2*m - 4)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
