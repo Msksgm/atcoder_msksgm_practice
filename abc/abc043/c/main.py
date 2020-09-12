92bebe4ff73ff2e73cfc60a116cdf8a6c2d4955c
Msksgm <4419517@ed.tus.ac.jp>
2020-09-12 13:40:56 +0900

    ac

---
+++ abc/abc043/c/main.py
@@ -0,0 +1,15 @@
+def main():
+    n = int(input())
+    As = list(map(int, input().split()))
+    ans = float("inf")
+
+    for i in range(-100, 101):
+        ans_temp = 0
+        for j in range(n):
+            ans_temp += (As[j] + i)**2
+        ans = min(ans, ans_temp)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
